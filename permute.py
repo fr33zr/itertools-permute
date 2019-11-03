d94c1f1bdfe0b4974e2cc0d7f39cb6f5a23d9090
opprssr <47411492+fr33zr@users.noreply.github.com>
2019-11-03 10:39:44 -0800

    Update permute.py

--- permute.py
+++ permute.py
@@ -3,5 +3,6 @@ from itertools import permutations
 input1 = input("Enter a word to receive all the possibilities!! > ")
 input1 = [''.join(z) for z in permutations(input1)]
 
-print (input1)
+print (input1, "\n")
+print ("[*] Number of outcomes are printed below [*]")
 print (len(input1))
